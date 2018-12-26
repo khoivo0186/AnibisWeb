05894b4b54322d9fa987c9a7dfe0e2a36402ac3a
Khoi Vo <khoivo@nvg.vn>
2018-12-26 17:24:42 +0700

    no message

---
+++ Interfaces/Login.js
@@ -0,0 +1,7 @@
+const txtEmail = '#ctl00_phlContent_ctlEmailValidationBox_txtEmail'
+const btnContinue = '#ctl00_phlContent_ctlEmailValidationBox_lbtContinue'
+const txtPassword = '#ctl00_phlContent_txtPassword'
+const btnLogin = '#ctl00_phlContent_btnLogin'
+module.exports = {
+  txtEmail, btnContinue, txtPassword, btnLogin
+}
--- Tests/BVT/SM01_Login.js
+++ Tests/BVT/SM01_Login.js
@@ -1,3 +1,5 @@
+import * as Login from '/Users/khoivo/AnibisWeb/Interfaces/Login'
+
 describe('SM01 - Login', function() {
 
   // TC: Verify user can login successful with valid account
@@ -8,8 +10,12 @@ describe('SM01 - Login', function() {
   //  - User login successful and correct username displays in Profile
   it('Verify user can login successful with valid account', function() {
     cy.visit("/")
-
     cy.contains("Se connecter").click()
+    cy.get(Login.txtEmail).type("reseller.nvg@gmail.com")
+    cy.get(Login.btnContinue).click()
+    cy.get(Login.txtPassword).type("nvg07072012")
+    cy.get(Login.btnLogin).click()
+    cy.contains("Compte d’utilisateur").click()
+    cy.get('.block').find('strong').should('have.value', 'Reseller NVG')
   })
-
 })
